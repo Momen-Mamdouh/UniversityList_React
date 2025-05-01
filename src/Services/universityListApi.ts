da6a2153804a4eb589e194a20a4858e0fe6b2ca0
Mo'men Mamdouh <momenmamdouhw@gmail.com>
2025-05-01 13:51:43 +0300

    tryNewCallForListCommit

--- src/Services/universityListApi.ts
+++ src/Services/universityListApi.ts
@@ -5,7 +5,7 @@ export const useUniversityListQuery = function(country: string) {
     return useQuery({
         queryKey: ['universities', country],
         queryFn: async () => {
-        const res = await fetch(`https://universities.hipolabs.com/search?country=${country}`);
+        const res = await fetch(`/api/universities.ts`);
         if (!res.ok) throw new Error('Failed to fetch universities');
         return res.json();
         }
